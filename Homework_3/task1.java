5c02e6a4c50b7b1c6aef763aa0df776a29104461
Maksim Aleksanin <maksim.g00gle@gmail.com>
2023-03-13 16:26:40 +0400

    Homework 3, task 1 - solved in for cicle

---
+++ Homework_3/task1.java
@@ -0,0 +1,23 @@
+package Homework_3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/*
+ * Дан список ArrayList<Integer> целых чисел. Удалить из него нечетные числа. 
+ */
+
+public class task1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> lst = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        System.out.println(lst.toString());
+        removeEven(lst);
+        System.out.println(lst.toString());
+    }
+
+    public static void removeEven(ArrayList<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) % 2 != 0) list.remove(i--);
+        }
+    }
+}
