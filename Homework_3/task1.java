9f24d7c4f1a3ca16bfe0f6e8ddd2547a770a8b0d
Maksim Aleksanin <maksim.g00gle@gmail.com>
2023-03-13 16:49:49 +0400

    Task 1 - solved with iterator, rename basic functions

--- Homework_3/task1.java
+++ Homework_3/task1.java
@@ -2,6 +2,7 @@ package Homework_3;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 
 /*
  * Дан список ArrayList<Integer> целых чисел. Удалить из него нечетные числа. 
@@ -11,13 +12,21 @@ public class task1 {
     public static void main(String[] args) {
         ArrayList<Integer> lst = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
         System.out.println(lst.toString());
-        removeEven(lst);
+        // removeOdd(lst);
+        removeOddIt(lst);
         System.out.println(lst.toString());
     }
 
-    public static void removeEven(ArrayList<Integer> list) {
+    public static void removeOdd(ArrayList<Integer> list) {
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i) % 2 != 0) list.remove(i--);
+        }
+    }
+
+    public static void removeOddIt(ArrayList<Integer> list) {
+        Iterator<Integer> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next() % 2 != 0) iterator.remove();
         }
     }
 }
